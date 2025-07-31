9046e784182a6df5799bb31dcccd99ffc73bb90c
josue <josue@DESKTOP-09BVU1G.>
2025-07-31 15:10:24 +0200

    WIP

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/jpradass/circe
 
 go 1.24.4
 
-require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-)
+require github.com/fsnotify/fsnotify v1.9.0
+
+require golang.org/x/sys v0.13.0 // indirect
--- pkg/watcher.go
+++ pkg/watcher.go
@@ -4,25 +4,28 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 type Watcher struct {
-	filepaths []string
+	filepaths   []string
+	destination string
 }
 
-func NewWatcher(filepaths []string) *Watcher {
+func NewWatcher(filepaths []string, destination string) *Watcher {
 	return &Watcher{
 		filepaths,
+		destination,
 	}
 }
 
 func (w *Watcher) Init() error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		return fmt.Errorf("error when creating fsnotify watcher. Details: %s", err.Error())
+		return fmt.Errorf("error when creating fsnotify watcher. Details: %s\n", err.Error())
 	}
 
 	defer watcher.Close()
@@ -34,16 +37,39 @@ func (w *Watcher) Init() error {
 				if !ok {
 					return
 				}
+				fmt.Printf("received event: %v", event)
 
-				if event.Op&fsnotify.Write == fsnotify.Write {
+				fileinfo, err := os.Stat(event.Name)
+				if err != nil {
+					fmt.Printf("there was an error getting stat of file. Details: %s\n", err.Error())
+					return
+				}
+
+				if event.Has(fsnotify.Write) {
+					if fileinfo.IsDir() {
+						// look for file inside dir
+					} else {
+						// We move file to destination
+						err := os.Rename(event.Name, fmt.Sprintf("%s/%s", w.destination, filepath.Base(event.Name)))
+						if err != nil {
+							fmt.Printf("there was an error moving file %s to destination %s. Details: %s\n", filepath.Base(event.Name), w.destination, err.Error())
+							return
+						}
+
+						// When move is done, we need to warn the other part
+					}
 					// do something
+				}
+
+				if event.Has(fsnotify.Create) {
+					// do something else
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
 				}
 
-				fmt.Printf("there was an error on watcher. Details: %s", err.Error())
+				fmt.Printf("there was an error on watcher. Details: %s\n", err.Error())
 				// print something
 			}
 		}
